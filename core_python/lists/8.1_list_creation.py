615cddee5b4397e325df6847ae466c09e7b4105b
Viswanadh <77051697+viswa199@users.noreply.github.com>
2025-06-26 12:45:12 +0530

    Create 8.1_list_creation.py

---
+++ core_python/lists/8.1_list_creation.py
@@ -0,0 +1,16 @@
+# A list in Python is like a box that can hold many things together.
+# You can keep numbers, words, or a mix of both inside a list.
+# We can make a list by using square brackets [ ] and putting items inside, separated by commas.
+# Loop through the list and print each item on line 11.
+
+# Task 1 : Create a list of numbers from 23 to 28 on line 17.
+# Task 2 : Write a for loop to print each item of the number list on line 19.
+
+# Here, fruits is a list that contains three items: apple, banana, and cherry
+fruits = ["apple", "banana", "cherry"]
+print(fruits)
+
+# Loop through the list and print each item
+for fruit in fruits:
+    print(fruit)
+
