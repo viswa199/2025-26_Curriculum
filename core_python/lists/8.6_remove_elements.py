cf622768089ea3f122b5a5b41c5efebf74d13863
Viswanadh <77051697+viswa199@users.noreply.github.com>
2025-06-26 13:58:32 +0530

    Update and rename 8.5_remove_elements.py to 8.6_remove_elements.py

---
+++ core_python/lists/8.6_remove_elements.py
@@ -0,0 +1,30 @@
+# Here, we created a list named lst and stored some intergers in it.
+# The "remove" method on a list removes the first occurence of the value in that list.
+# i.e., if you called lst.remove(1), it removes the first occurence of 1 from the list.
+
+# The len function returns the length of the list.
+
+#Task1: Remove the element 6 from the list on line 27.
+#Task2: Try to remove the element 7 from the list and observe the error on line 31.
+
+lst = [1, 2, 3, 4, 5, 6]
+print(lst) #[1, 2, 3, 4, 5, 6]
+print(len(lst)) #6
+print('-------------')
+
+lst.remove(1)
+print(lst) #[2, 3, 4, 5, 6]
+print(len(lst)) #5
+print('-------------')
+
+lst.remove(2)
+print(lst) #[3, 4, 5, 6]
+print(len(lst)) #4
+print('-------------')
+
+#Task1
+#remove the element and print the list
+
+
+#Task2
+#try to remove the element that does not exist in the list.
