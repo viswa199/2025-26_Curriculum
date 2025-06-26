0be8631c673770d801d1dc58a9f77ae0f79d071b
Viswanadh <77051697+viswa199@users.noreply.github.com>
2025-06-26 12:51:49 +0530

    Update 8.2_range.py

--- core_python/lists/8.2_range.py
+++ core_python/lists/8.2_range.py
@@ -1,20 +1,20 @@
-#Task: Create odd numbers between 0 and 100.
+# range will generate a sequence of numbers in the given range on line 8.
+# range can have three arguments: start(optional), stop, step(optional) on line 16.
 
-'''
-range will generate a sequence of numbers in the given range.
-range can have three arguments: start(optional), stop, step(optional)
-If you only provided one argument to the range function, it is stop. start has default value of 0 and step has default value of 1.
-range is exclusive of stop, it does not include stop value.
-'''
+#Task: Create odd numbers between 0 and 100 using range on line 21 and print it.
 
-#We only provided one argument to the range, 10 is the value of stop. start & step have default values.
+#We only provided one argument to the range, 10 is the value of stop.
+# start & step have default values.
 numbers = list(range(10))
 print(numbers)
 
-#We provided two arguments to the range, 1 and 10 are the values of start and stop respectively. step has the default value.
+#We provided two arguments to the range,
+# 1 and 10 are the values of start and stop respectively. step has the default value.
 numbers = list(range(1, 10))
 print(numbers)
 
-#We provided three arguments to the range, 0, 10, 2 are the values of start, stop and step respectively.
+#We provided three arguments to the range, 0, 10, 2 are the values of start,
+# stop and step respectively.
 even_numbers = list(range(0, 10, 2))
 print(even_numbers)
+
